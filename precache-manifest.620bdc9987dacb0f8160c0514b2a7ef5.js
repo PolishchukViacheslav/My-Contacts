c114797cb166bcff7f39122f94ceb3d11107d429
Slava Polishchuk <dizerllc@gmail.com>
2020-09-22 23:17:35 +0300

    Updates

---
+++ precache-manifest.620bdc9987dacb0f8160c0514b2a7ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d23c7d03980240beb6c0d39477beb51",
+    "url": "/myContacts/index.html"
+  },
+  {
+    "revision": "d90cfc0ec3842ebc0828",
+    "url": "/myContacts/static/css/main.968fa37a.chunk.css"
+  },
+  {
+    "revision": "a3ed8ac62b1c122682ec",
+    "url": "/myContacts/static/js/2.b05a785f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myContacts/static/js/2.b05a785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90cfc0ec3842ebc0828",
+    "url": "/myContacts/static/js/main.77a7209a.chunk.js"
+  },
+  {
+    "revision": "f68e909a417722d06ca5",
+    "url": "/myContacts/static/js/runtime-main.780e24c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myContacts/precache-manifest.6fe3549f8a62b4d9c355ad0959e0860e.js"
+  "/myContacts/precache-manifest.620bdc9987dacb0f8160c0514b2a7ef5.js"
 );
 
 self.addEventListener('message', (event) => {
